59fb68a99a1478e904bd1960230d08b8eba88a1d
mikehudz <mikehudz7@hotmail.com>
2026-01-29 02:43:37 -0700

    Add negative login test and fix checkout locators

--- tests/smoke/login.spec.js
+++ tests/smoke/login.spec.js
@@ -1,6 +1,21 @@
 const { test, expect } = require('@playwright/test');
 const { LoginPage } = require('../../pages/LoginPage');
 const { users } = require('../../utils/users');
+const invalidUsers = [
+  { username: 'wrong_user', password: 'secret_sauce', description: 'incorrect username' },
+  { username: 'standard_user', password: 'wrong_password', description: 'incorrect password' },
+];
+
+invalidUsers.forEach(({ username, password, description }) => {
+  test(`login fails with ${description}`, async ({ page }) => {
+    const login = new LoginPage(page);
+    await login.goto();
+    await login.login(username, password);
+
+    const error = page.locator('[data-test="error"]');
+    await expect(error).toHaveText(/Epic sadface/i);
+  });
+});
 
 test('standard user can log in', async ({ page }) => {
   const login = new LoginPage(page);
